be207472b6b9c23ccd08a1eb3bf975f5e3ec487d
Harvey <harveyjwilliams@aol.co.uk>
2019-06-10 23:07:56 +0200

    Initial.

---
+++ Source/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BaseService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Source/SimpleMethod.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaseService
+{
+    public class SimpleClass
+    {
+        public void SimpleMethod(EntityState entityState = EntityState.Added)
+        {
+
+        }
+    }
+}
